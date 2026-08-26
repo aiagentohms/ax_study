fa87adc73aac4088e7a55a0855df82cee50a82ce
[aiagentohms] <[mangostick112@gmail.com]>
2026-08-26 16:10:24 +0900

    260800 파이썬 공부

---
+++ 02_PYTHON/chapter02/01.py
@@ -0,0 +1,4 @@
+import sys
+
+args = sys.argv[1:]
+print(int(args[0]) + int(args[1]))
---
+++ 02_PYTHON/chapter06/mod1.py
@@ -0,0 +1,12 @@
+def add(num1, num2):
+    return num1 + num2
+
+def minus(num1, num2):
+    return num1 - num2
+
+VERSION = "1.0.0"
+
+print("모듈명:", __name__)
+
+result = add(10, 20)
+print("결과: ", result)
